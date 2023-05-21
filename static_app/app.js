8773568877296759fc54b2e1f1052dbc994b1885
deveshbedmutha1406 <deveshbedmutha500@gmail.com>
2023-05-22 00:44:18 +0530

    modified app.js

--- static_app/app.js
+++ static_app/app.js
@@ -1,9 +1,8 @@
 const express = require("express");
+const path = require('path');
 const app = express();
 const port = 3001;
-
-
-app.use(express.static('public'));
+app.use(express.static(path.join(__dirname, "./public")))
 
 app.listen(port, () => {
     console.log(`listening on port ${port}`);
